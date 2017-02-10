edd3566141a1cc6ed9d2af1adf1e7390fad2a543
andrey.malyshenko <andrey.malyshenko@gmail.com>
2017-02-10 18:22:16 +0000

    added env vars to change frontend name and port

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,15 @@ RUN apt-get update && apt-get install git build-essential uuid-dev psmisc -y
 
 RUN cd / && git clone https://github.com/anabolyc/xupnpd.git
 RUN cd /xupnpd/src && make
+RUN mkdir /xupnpd/src/playlists -p
 
 # cleanup 
 RUN apt-get remove git build-essential uuid-dev psmisc -y && apt-get autoremove -y
 RUN rm -rf /var/lib/apt/lists/*
+
 ENV M3U_URL http://example.com/list.m3u
+ENV FRONTEND_PORT 4044
+ENV FORNTEND_NAME IPTV
 
 COPY ./start.sh /usr/sbin/start.sh
 
--- start.sh
+++ start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-url=
+# patch config
 
-while getopts u: opt; do
-  case $opt in
-  u)
-      url=$OPTARG
-      ;;
-  esac
-done
+if [ ! -f /xupnpd/src/.xupnpd.lua.patched ]; then
+	echo "Patching configuration file"
+	sed -e "s/UPnP-IPTV/${FRONTEND_NAME}/" -e "s/4044/${FRONTEND_PORT}/" -i /xupnpd/src/xupnpd.lua
+	touch /xupnpd/src/.xupnpd.lua.patched
+else
+	echo "Config file appears to be patched already"
+fi
 
 # update data
 echo "Starting data update from $M3U_URL every 300 seconds"
 while true
 do
-	timeout -sHUP 1m wget "$M3U_URL" -P /xupnpd/src/playlists/
 	sleep 300
+	timeout -sHUP 1m wget "$M3U_URL" -O /xupnpd/src/playlists/list.m3u
 done &
 
-/xupnpd/src/xupnpd
+wget "$M3U_URL" -O /xupnpd/src/playlists/list.m3u && /xupnpd/src/xupnpd
 
