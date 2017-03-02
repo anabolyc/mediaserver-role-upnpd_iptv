d0a922660c7f42e21360a7e3ce0257ee2ddbebb7
andrey.malyshenko <andrey.malyshenko@gmail.com>
2017-03-02 21:49:53 +0000

    names fixing

--- d_run.sh
+++ d_run.sh
@@ -1,1 +1,1 @@
-docker run --rm -ti --net=host --name upnpd_iptv-instance --env FRONTEND_PORT=4044 --env FRONTEND_NAME=IPTV --env BACKEND_GUID=61cfd247-be43-4736-8344-bd0fa2100f8e $(cat tag)
+docker run --rm -ti --net=host --name upnpd_iptv-instance --env FRONTEND_PORT=4044 --env FRONTEND_NAME=IPTV-DEV --env BACKEND_GUID=61cfd247-be43-4736-8344-bd0fa2100f8e $(cat tag)
